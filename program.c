9436cde7a1703f9ad5da849aadd335f642bd958e
ZlfDino <1210923@teslabg.edu.rs>
2026-04-09 03:09:44 -0700

    Ovo je DOBRA izmena

--- program.c
+++ program.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main(){
-  printf("Ti si nadaren koliko skolovani prevarant,\n Imao si nisi znao\n kako sjaji dijamant \n a ja samo sam \n htjela oca djeci malo ljubavi\n Pa ti samo reciiiii \n Ja sam ti bila privjezak sto se sija \n Obmana il kobpija \n mome srcu \n samo bizuterija ");
+  printf("Ti si nadaren koliko skolovani prevarant,\n Imao si nisi znao\n
+     kako sjaji dijamant \n a ja samo sam \n htjela oca djeci malo ljubavi\n Pa ti samo reciiiii 
+     \n Ja sam ti bila privjezak sto se sija \n Obmana il kobpija \n mome srcu \n samo bizuterija
+     \n a bila sam ti sudjena \n blago meni \n jos po sebi \n tvoje prste osjecam ");
   printf("Jelena rozga ludilo mozga");
   printf("Menjao sam zbog nje fazon pustio sam bradu");
   return 0;

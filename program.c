5abf5cc228e9073b2e2625b323fe3c81d0d0976c
ZlfDino <1210923@teslabg.edu.rs>
2026-04-09 12:15:33 +0200

    Update program.c
    joj

--- program.c
+++ program.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main(){
+int main(){ //asdasdasdasdasdasdasdasdasdsa
   printf("Ti si nadaren koliko skolovani prevarant,\n Imao si nisi znao\n
      kako sjaji dijamant \n a ja samo sam \n htjela oca djeci malo ljubavi\n Pa ti samo reciiiii 
      \n Ja sam ti bila privjezak sto se sija \n Obmana il kobpija \n mome srcu \n samo bizuterija
